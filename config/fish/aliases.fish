c7c0045ddf475b99f5e8a04961758d7fb309673b
Reda Lemeden <reda@thoughtbot.com>
2014-05-14 19:27:02 +0200

    Update fixopenwith

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -5,7 +5,7 @@ alias aliases "mvim ~/.config/fish/aliases.fish"
 alias sshnfs "ssh kaishin_kaishinlab@ssh.phx.nearlyfreespeech.net"
 alias rsync "/usr/local/bin/rsync"
 alias tower "gittower"
-alias fixopenwith '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user | killall Finder'
+alias fixopenwith '/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user'
 alias unfollow "t leaders | xargs t unfollow >/dev/null"
 alias oculair "cd ~/Developer/iOS/oculair"
 alias verbena "cd ~/Developer/iOS/Verbena | xcode"

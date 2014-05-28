aa4bde2ce2f33f28250a9046d9966934adaee054
Reda Lemeden <reda@thoughtbot.com>
2014-05-29 01:52:39 +0200

    Update fish aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,18 +1,18 @@
-alias migrate "rake db:migrate"
-alias mkdir "mkdir -p"
-alias ... "../.."
-alias aliases "mvim ~/.config/fish/aliases.fish"
-alias sshnfs "ssh kaishin_kaishinlab@ssh.phx.nearlyfreespeech.net"
-alias rsync "/usr/local/bin/rsync"
-alias tower "gittower"
+alias migrate 'rake db:migrate'
+alias mkdir 'mkdir -p'
+alias ... '../..'
+alias aliases 'mvim ~/.config/fish/aliases.fish'
+alias sshnfs 'ssh kaishin_kaishinlab@ssh.phx.nearlyfreespeech.net'
+alias rsync '/usr/local/bin/rsync'
+alias tower 'gittower'
 alias fixopenwith '/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user'
-alias unfollow "t leaders | xargs t unfollow >/dev/null"
-alias oculair "cd ~/Developer/iOS/oculair"
-alias verbena "cd ~/Developer/iOS/Verbena | xcode"
-alias ios-playground "cd ~/Developer/iOS/Playground | xcode"
-alias neat-repo "cd ~/Developer/libs/neat"
+alias unfollow 't leaders | xargs t unfollow >/dev/null'
+alias oculair 'cd ~/Developer/iOS/oculair'
+alias verbena 'cd ~/Developer/iOS/Verbena | xcode'
+alias ios-playground 'cd ~/Developer/iOS/Playground | xcode'
+alias neat-repo 'cd ~/Developer/libs/neat'
 alias xcode "open -a 'Xcode' ."
-alias kaishinlab "cd ~/Developer/web/kaishinlab"
+alias kaishinlab 'cd ~/Developer/web/kaishinlab'
 alias bower 'bower'
 alias pow 'powder'
 alias gs 'gitsh --git (which gh)'

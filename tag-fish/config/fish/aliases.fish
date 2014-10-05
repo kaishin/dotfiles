15e2de498959ac929cb27566c386358fb74393cd
Reda Lemeden <reda@thoughtbot.com>
2014-10-05 22:16:55 +0200

    Use rcm tags & cleanup fish config

---
+++ tag-fish/config/fish/aliases.fish
@@ -0,0 +1,6 @@
+alias ... '../..'
+alias aliases 'vim ~/.config/fish/aliases.fish'
+alias finder 'open .'
+alias mkdir 'mkdir -p'
+alias rsa 'cat ~/.ssh/id_rsa.pub | pbcopy'
+alias xcode "open -a 'Xcode' ."
--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -2,17 +2,8 @@ set FISH $HOME/.config/fish
 . $FISH/aliases.fish
 
 set -x PATH /usr/X11/bin $PATH
-set -x PATH /usr/local/git/bin $PATH
-set -x PATH /usr/local/heroku/bin $PATH
 set -x PATH /usr/bin $PATH
 set -x PATH /bin $PATH
 set -x PATH /usr/sbin $PATH
 set -x PATH /sbin $PATH
 set -x PATH /usr/local/bin $PATH
-
-set -x EDITOR /usr/local/bin/mvim
-set -x VISUAL /usr/local/bin/mvim
-
-set -x PGHOST localhost
-
-rvm use default >/dev/null

9e637effbe63517944b77c1ba72410b3afa83fbd
Reda Lemeden <reda@thoughtbot.com>
2014-10-05 23:38:46 +0200

    Update prompt

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -10,19 +10,6 @@ function _git_is_dirty
 end
 
 function fish_prompt
-    set -l last_status $status
-
-    set -l red (set_color red)
-    set -l green (set_color b5bd68)
-    set -l cyan (set_color cyan)
-    set -l black (set_color black)
-    set -l bg_blue (set_color -b blue)
-    set -l bg_cyan (set_color -b cyan)
-    set -l bg_white (set_color -b white)
-    set -l bg_red (set_color -b red)
-    set -l bg_yellow (set_color -b yellow)
-    set -l normal (set_color normal)
-
     set -l blue (set_color -o blue)
     set -l cyan (set_color -o cyan)
     set -l green (set_color -o green)
@@ -33,26 +20,27 @@ function fish_prompt
     set -l yellow (set_color -o yellow)
     
     set -l cwd $cyan(basename (prompt_pwd))
+    set -l last_status $status
     set -l uid (id -u $USER)
 
     # Display current path
-    echo -n -s $cyan " $cwd " $green
+    echo -n -s $cyan "$cwd" $green
 
     # Show git branch and dirty state
     if [ (_git_branch_name) ]
         set -l git_branch (_git_branch_name)
 
         if [ (_git_is_dirty) ]
-            echo -n -s $normal "$git_branch " $red "$_flag " $normal
+            echo -n -s " $white($normal$git_branch$white) " $red "$_flag" $normal
         else
-            echo -n -s $normal " $git_branch " $normal
+            echo -n -s " $white($normal$git_branch$white)" $normal
         end
     end
 
     # Show glyph
     if test $last_status -ne 0
-        echo -n -s $red "$_glyph "  $normal
+        echo -n -s $red " $_glyph "  $normal
     else
-        echo -n -s $white "$_glyph " $normal
+        echo -n -s $white " $_glyph " $normal
     end
 end

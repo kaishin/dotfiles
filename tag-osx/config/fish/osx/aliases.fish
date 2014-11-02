daac2a32ba9b515e7d7ba0ca2fc8cd04d09ccd90
Reda Lemeden <reda@thoughtbot.com>
2014-11-02 16:30:29 +0100

    Add Tower alias

--- tag-osx/config/fish/osx/aliases.fish
+++ tag-osx/config/fish/osx/aliases.fish
@@ -1,3 +1,4 @@
-alias finder 'open .'
-alias rsa 'cat ~/.ssh/id_rsa.pub | pbcopy'
+alias finder "open ."
+alias rsa "cat ~/.ssh/id_rsa.pub | pbcopy"
 alias xcode "open -a 'Xcode' ."
+alias tower "gittower ./"

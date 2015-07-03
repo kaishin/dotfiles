dbc9586425dc03e2f4c582c900e9ab7c35ec56b5
Reda Lemeden <reda@thoughtbot.com>
2015-07-03 21:07:47 +0200

    Use rbenv in OS X only

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -5,14 +5,14 @@ set FISH $HOME/.config/fish
 if test -d $FISH/osx
   . $FISH/osx/aliases.fish
   set -x PATH /usr/X11/bin $PATH
+  set PATH $HOME/.rbenv/bin $PATH
+  set PATH $HOME/.rbenv/shims $PATH
 end
 
 if test -d $FISH/server
   . $FISH/server/aliases.fish
 end
 
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
 set PATH $HOME/.node/bin $PATH
 
 set EDITOR '/usr/bin/vim'

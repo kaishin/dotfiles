6ab49fdca8b0c2968f68c3f10833d7e6539c361a
Reda Lemeden <git@kaishin.haz.email>
2015-10-06 20:29:50 +0100

    Add permission fix alias for El Capitan

--- tag-osx/config/fish/osx/aliases.fish
+++ tag-osx/config/fish/osx/aliases.fish
@@ -2,6 +2,7 @@ alias ssh-server "ssh server.redalemeden.com"
 alias osxinfo "sw_vers"
 alias rsa "cat ~/.ssh/id_rsa.pub | pbcopy"
 alias git "gh"
+alias permission-fix "sudo chown -R (whoami):admin /usr/local"
 
 alias finder "open ."
 alias subl "sublime ./"

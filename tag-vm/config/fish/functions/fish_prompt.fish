7fa37fc2eb5e6108fbef9b0a8819af02d955971f
Reda Lemeden <git@kaishin.haz.email>
2018-11-18 20:14:37 +0100

    Remove tag-vm

--- tag-vm/config/fish/functions/fish_prompt.fish
+++
@@ -1,9 +0,0 @@
-set _glyph \u27E9\u27E9
-
-function fish_prompt
-    set -l yellow (set_color -o yellow)
-    set -l normal (set_color normal)
-
-    set -l cwd $yellow(basename (prompt_pwd))
-    echo -n "$cwd $_glyph" $normal
-end

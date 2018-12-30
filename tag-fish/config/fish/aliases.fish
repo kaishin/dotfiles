fbe18155838adf696d1cb03a1048ca87558bb326
Reda Lemeden <git@kaishin.haz.email>
2018-12-30 22:27:34 +0100

    Add fish aliases for Docker

--- tag-fish/config/fish/aliases.fish
+++ tag-fish/config/fish/aliases.fish
@@ -4,3 +4,7 @@ alias mkdir "mkdir -p"
 alias rm "rm -rf"
 alias port "lsof -i"
 alias killpid "kill -9"
+alias gitc "git aa | git c"
+alias dockcont "docker container"
+alias dockcomp "docker-compose"
+

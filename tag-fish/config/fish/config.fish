71014006addf8af3027efe115be72ddb0fb76644
Reda Lemeden <git@kaishin.haz.email>
2019-09-10 21:11:37 +0200

    Add linuxbrew and swift paths

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -16,7 +16,24 @@ end
 if test -d $FISH/server
   . $FISH/server/aliases.fish
   fundle plugin 'danhper/fish-ssh-agent'
-  fundle plugin 'oh-my-fish/plugin-linuxbrew'
+  set -l linuxbrew_bin_path "/home/linuxbrew/.linuxbrew/bin"
+  set -l linuxbrew_sbin_path "/home/linuxbrew/.linuxbrew/sbin"
+  set -l linuxbrew_manpath "/home/linuxbrew/.linuxbrew/share/man"
+  set -l linuxbrew_infopath "/home/linuxbrew/.linuxbrew/share/info"
+
+  contains -- $linuxbrew_bin_path $PATH
+    or set -gx PATH $linuxbrew_bin_path $PATH
+
+  contains -- $linuxbrew_sbin_path $PATH
+    or set -gx PATH $linuxbrew_sbin_path $PATH
+
+  contains -- $linuxbrew_manpath $MANPATH
+    or set -gx MANPATH $linuxbrew_manpath $MANPATH
+
+  contains -- $linuxbrew_infopath $INFOPATH
+    or set -gx INFOPATH $linuxbrew_infopath $INFOPATH
+    
+  set PATH $HOME/swift $PATH
 end
 
 set EDITOR '/usr/bin/vim'

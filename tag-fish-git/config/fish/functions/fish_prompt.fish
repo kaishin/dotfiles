4ad0dc2f5511858d2b41c9d6b951f9d10467fa8d
Reda Lemeden <git@redalemeden.com>
2021-12-16 00:15:20 +0100

    Update fish error redirection syntax https://stackoverflow.com/a/68457230/656507

--- tag-fish-git/config/fish/functions/fish_prompt.fish
+++ tag-fish-git/config/fish/functions/fish_prompt.fish
@@ -2,11 +2,11 @@ set _glyph \u27E9\u27E9
 set _flag \u2691
 
 function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+  echo (command git status -s --ignore-submodules=dirty 2>/dev/null)
 end
 
 function fish_prompt

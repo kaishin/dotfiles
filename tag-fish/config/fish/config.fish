3622e65ecdd9cd7ce722144f5834500b5363b2ed
Reda Lemeden <git@redalemeden.com>
2022-04-09 23:46:30 +0000

    Unify macOS tags

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -2,32 +2,22 @@ set FISH $HOME/.config/fish
 . $FISH/aliases.fish
 . $FISH/colors.fish
 
-if test -d $FISH/arm-mac
-  . $FISH/arm-mac/aliases.fish
-  set PATH /opt/homebrew/bin $PATH
-  set PATH /opt/homebrew/sbin $PATH
-  set EDITOR '/opt/homebrew/bin/nvim'
-end
+if test -d $FISH/macos
+  set -gx PNPM_HOME $HOME/Library/pnpm
+  
+  . $FISH/macos/aliases.fish
+  fish_add_path /opt/homebrew/bin
+  fish_add_path /opt/homebrew/sbin
+  fish_add_path "$PNPM_HOME"
 
-if test -d $FISH/osx
-  . $FISH/osx/aliases.fish
   status --is-interactive; and . (swiftenv init -|psub)
-  set -x GOPATH $HOME/.golang
-  set -x GOROOT /usr/local/opt/go/libexec
-  set PATH $GOPATH/bin $GOROOT/bin $PATH
-  set PATH $HOME/.node/bin $PATH
-  set PATH $HOME/.rbenv/shims $PATH
-  set PATH $HOME/.cargo/bin $PATH
-  set PATH /usr/local/sbin $PATH
-  set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig"
-  set EDITOR '/usr/bin/vim'
+  set EDITOR '/opt/homebrew/bin/nvim'
 end
 
 if test -d $FISH/server
   . $FISH/server/aliases.fish
   set -gx PATH $HOME/.swift/usr/bin $PATH
   
-
   set -l linuxbrew_bin_path "/home/linuxbrew/.linuxbrew/bin"
   set -l linuxbrew_sbin_path "/home/linuxbrew/.linuxbrew/sbin"
   set -l linuxbrew_manpath "/home/linuxbrew/.linuxbrew/share/man"
@@ -45,7 +35,6 @@ if test -d $FISH/server
   contains -- $linuxbrew_infopath $INFOPATH
     or set -gx INFOPATH $linuxbrew_infopath $INFOPATH
 end
-
 
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8

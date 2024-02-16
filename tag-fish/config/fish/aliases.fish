d113274c6e24d657a8b70bc8ee9a36b2fc21ff62
Reda Lemeden <git@redalemeden.com>
2024-02-16 22:07:46 +0100

    Update macOS aliases

--- tag-fish/config/fish/aliases.fish
+++ tag-fish/config/fish/aliases.fish
@@ -1,2 +1,4 @@
 alias ... "cd ../.."
-
+alias dockcont "docker container"
+alias dockcomp "docker-compose"
+alias remove-local-branches "git branch -vv | grep ': gone]' | awk '{ print \$1 }' | xargs git branch -D"

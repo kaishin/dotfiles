559cdd056a507782b6f3547dac42d7eb30fd1949
Reda Lemeden <git@redalemeden.com>
2025-05-18 12:21:41 +0200

    Added Orbstack integration

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -40,3 +40,7 @@ fundle init
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :

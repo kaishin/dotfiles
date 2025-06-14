7265aa95ad624bda8a6ba06805e33080bd630c07
Reda Lemeden <git@redalemeden.com>
2025-06-14 13:48:13 +0200

    Add claude alias

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -46,3 +46,5 @@ set --export PATH $BUN_INSTALL/bin $PATH
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+alias claude="/Users/kaishin/.claude/local/claude"
